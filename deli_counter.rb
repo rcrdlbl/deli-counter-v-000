ae6504ab2c1c30d58635a466ebb3a1b87914a18d
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-30 01:23:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 katz_deli = []
-def line
-  if katz_deli.length == 0
+def line(names)
+  if names.length == 0
     puts "The line is currently empty."
   else
     announcement = "The line is currently:"
-    katz_deli.each.with_index(1) do |name, number|
+    names.each.with_index(1) do |name, number|
       announcement << " #{number}. #{name}"
     end
     return announcement
