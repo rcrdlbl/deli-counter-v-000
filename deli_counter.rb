7d1d52ca1977e25ed9a849f23ad07d9bc0a9b9b4
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-30 01:21:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli = []
+def line
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    announcement = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, number|
+      announcement << " #{number}. #{name}"
+    end
+    return announcement
+  end
+end
+    
